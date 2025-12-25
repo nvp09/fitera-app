830a72749b36817fb4faf48731367ba26a98fda4
Nvp.Kcn <nvp.kcn@gmail.com>
2025-12-25 16:08:37 +0700

    feat/app: initial react app setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-headline-1">
+        My First React App on Vite
+      </h1>
+    </section>
+  )
+}
+
+export default App
